dff17d55d184ccd641106123486f8e19a417355e
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-03-18 15:34:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// return first two drivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // return last two drivers
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // array of two functions: returnFirstTwoDrivers and returnLastTwoDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // create a fare multiplier function
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // select different drivers
+  const selectDifferentDrivers = function(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  };
+  
